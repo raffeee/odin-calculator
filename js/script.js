fa4cd24dd050918f2f6f72a0336a903bac96c01c
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-09-04 15:31:47 +0800

    fix: evaluate only one pair of numbers at a time
    In the sequence (number, operator, number, operator...) the program should operate on the two numbers before calculating with the next operator.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 function change_display(value) {
-  const display = document.querySelector("#display")
-  if (display.textContent == 0) {
-    display.textContent = value
+  const display = document.querySelector("#display");
+  if (evaluate == true) {
+    display.textContent = value;
+    evaluate = false;
+  } else if (display.textContent == 0 | res == true) {
+    display.textContent = value;
+    res = false;
   } else {
-    display.textContent += value
+    display.textContent += value;
   }
 }
 
@@ -32,10 +36,69 @@ function operate(optr, a, b) {
   else if (optr === '-') return subtract(a, b);
   else if (optr === '*') return multiply(a, b);
   else if (optr === '/') return divide(a, b);
-  else return 0
+  else return 0;
 }
 
-let num1 = 0
-let num2 = 0
-let operator = ''
-let curr = 0
+function reset_calc() {
+  num1 = 0;
+  num2 = 0;
+  optr = '';
+  last_action = '';
+  res = true;
+  change_display(0);
+  res = false;
+  evaluate = false;
+}
+
+let num1 = 0;
+let num2 = 0;
+let optr = '';
+let last_action = '';
+let res = false;
+let evaluate = false;
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    change_display(number.id)
+  });
+});
+
+const operators = document.querySelectorAll(".operator")
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    optr = operator.textContent;
+    last_action = optr;
+    res = true;
+    if (num1) {
+      if (last_action != '=') {
+        num2 = parseInt(document.querySelector("#display").textContent);
+      }
+      evaluate = true;
+      result = operate(optr, num1, num2);
+      change_display(result);
+      num1 = result;
+    } else {
+      num1 = parseInt(document.querySelector("#display").textContent);
+    }
+  });
+});
+
+document.querySelector("#equals").addEventListener("click", () => {
+  if (num1) {
+    if (last_action != '=') {
+      num2 = parseInt(document.querySelector("#display").textContent);
+    }
+  } else {
+    num1 = parseInt(document.querySelector("#display").textContent);
+  }
+  evaluate = true;
+  result = operate(optr, num1, num2);
+  change_display(result);
+  num1 = result;
+  last_action = '=';
+})
+
+document.querySelector("#allclear").addEventListener("click", () => {
+  reset_calc();
+});
